d5b29d0d2171e0c4c72579307cb090436641ef7d
aymansainshy <aymansainshy@gmail.com>
2021-10-10 08:29:46 +0200

    Add Type of exports modules in node.js

---
+++ lesson3/typeOfExportsFile.js
@@ -0,0 +1,13 @@
+module.exports = ayman;
+
+module.exports = {
+    ayman: ayman,
+    someText: "some Dummy Text",
+}
+
+module.exports.ayman = ayman;
+module.exports.someText = "some Dummy Text";
+
+
+exports.ayman = ayman;
+exports.someText = "some Dummy Text";
