efb88d85ac4d8ae6b8c17b89cf9246d10dd96d56
aymansainshy <aymansainshy@gmail.com>
2021-10-21 21:00:40 +0200

    Add Product Details View

--- lesson4 - shopApp/controllers/shop.js
+++ lesson4 - shopApp/controllers/shop.js
@@ -12,7 +12,20 @@ exports.getAllProducts = (req, res, next) => {
 }
 
 
-exports.getIndex = (req, res, next) => { 
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        console.log(product);
+        res.render('shop/product-detail', {
+            pageTitle: 'P - Details',
+            product: product,
+            path: '/products',
+        });
+    });
+
+}
+
+exports.getIndex = (req, res, next) => {
     Product.fetchAll((products => {
         res.render('shop/index', {
             pageTitle: 'Shop',
